65e6b91a266648c9ecf07ed07406f3b78ca522a2
verekonn <verekonn@yandex-team.ru>
2022-03-16 10:00:39 +0300

    tst

--- src/main/java/ru/taksebe/telegram/writeRead/constants/bot/ButtonNameEnum.java
+++ src/main/java/ru/taksebe/telegram/writeRead/constants/bot/ButtonNameEnum.java
@@ -4,9 +4,10 @@ package ru.taksebe.telegram.writeRead.constants.bot;
  * Названия кнопок основной клавиатуры
  */
 public enum ButtonNameEnum {
-    GET_TASKS_BUTTON("Создать файл с заданиями"),
-    GET_DICTIONARY_BUTTON("Скачать словарь"),
-    UPLOAD_DICTIONARY_BUTTON("Загрузить мой словарь"),
+    START_FIGHT_TASKS_BUTTON("Начать бой с кем-нибудь"),
+    GET_TASKS_BUTTON("Атака!!!"),
+    GET_DICTIONARY_BUTTON("Разведка"),
+    UPLOAD_DICTIONARY_BUTTON("Защита!"),
     HELP_BUTTON("Помощь");
 
     private final String buttonName;
--- src/main/java/ru/taksebe/telegram/writeRead/telegram/keyboards/ReplyKeyboardMaker.java
+++ src/main/java/ru/taksebe/telegram/writeRead/telegram/keyboards/ReplyKeyboardMaker.java
@@ -17,16 +17,10 @@ public class ReplyKeyboardMaker {
 
     public ReplyKeyboardMarkup getMainMenuKeyboard() {
         KeyboardRow row1 = new KeyboardRow();
-        row1.add(new KeyboardButton(ButtonNameEnum.GET_TASKS_BUTTON.getButtonName()));
-        row1.add(new KeyboardButton(ButtonNameEnum.GET_DICTIONARY_BUTTON.getButtonName()));
-
-        KeyboardRow row2 = new KeyboardRow();
-        row2.add(new KeyboardButton(ButtonNameEnum.UPLOAD_DICTIONARY_BUTTON.getButtonName()));
-        row2.add(new KeyboardButton(ButtonNameEnum.HELP_BUTTON.getButtonName()));
+        row1.add(new KeyboardButton(ButtonNameEnum.START_FIGHT_TASKS_BUTTON.getButtonName()));
 
         List<KeyboardRow> keyboard = new ArrayList<>();
         keyboard.add(row1);
-        keyboard.add(row2);
 
         final ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
         replyKeyboardMarkup.setKeyboard(keyboard);
