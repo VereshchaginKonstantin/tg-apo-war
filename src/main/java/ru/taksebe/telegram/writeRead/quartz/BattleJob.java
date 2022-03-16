9b8812598f90d6c1c4e1fa3e1023f70ffe323c3a
verekonn <verekonn@yandex-team.ru>
2022-03-16 18:23:07 +0300

    tst

--- src/main/java/ru/taksebe/telegram/writeRead/quartz/BattleJob.java
+++ src/main/java/ru/taksebe/telegram/writeRead/quartz/BattleJob.java
@@ -4,11 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-import javax.annotation.Resource;
-
-import lombok.AccessLevel;
-import lombok.RequiredArgsConstructor;
-import lombok.experimental.FieldDefaults;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -19,12 +14,14 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.taksebe.telegram.writeRead.api.dictionaries.BattleRepository;
 import ru.taksebe.telegram.writeRead.api.dictionaries.UserRepository;
 import ru.taksebe.telegram.writeRead.model.Battle;
-import ru.taksebe.telegram.writeRead.telegram.TelegramApiClient;
 import ru.taksebe.telegram.writeRead.telegram.WriteReadBot;
 
 
 @Component
 public class BattleJob implements Job {
+
+    static Object lock =  new Object();
+    static Random random = new Random(2333);
 
     @Autowired
     WriteReadBot writeReadBot;
@@ -38,63 +35,66 @@ public class BattleJob implements Job {
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        List<Battle> remove = new ArrayList<>();
-        battleRepository.findAll().forEach(b -> {
-            b.setTime(b.getTime() + 1);
-            battleRepository.save(b);
-            var userFirst = userRepository
-                    .findById(b
-                            .getUserFirst()
-                            .getUserName())
-                    .get();
-            var userSecond = userRepository
-                    .findById(b
-                            .getUserSecond()
-                            .getUserName())
-                    .get();
-            String text = "В процессе нападения ";
-            if (b.getTime() > Battle.END) {
-                text = "Конец победил ";
-                Random r = new Random(2);
-                var i = r.nextInt();
-                boolean win = i % 2 == 0;
-                if (win) {
-                    text += userFirst.getUserName();
-                    userFirst.setWins(userFirst.getWins() + 1);
-                    userFirst.setCash(userFirst.getCash() + 100);
-                    userSecond.setCash(userSecond.getCash() - 100);
-                    userSecond.setLoose(userSecond.getLoose() + 1);
-                } else {
-                    text += userSecond.getUserName();
-                    userSecond.setWins(userSecond.getWins() + 1);
-                    userSecond.setCash(userSecond.getCash() + 100);
-                    userFirst.setCash(userFirst.getCash() - 100);
-                    userFirst.setLoose(userFirst.getLoose() + 1);
+        synchronized (lock) {
+            List<Battle> remove = new ArrayList<>();
+            battleRepository.findAll().forEach(b -> {
+                b.setTime(b.getTime() + 1);
+                battleRepository.save(b);
+                var userFirst = userRepository
+                        .findById(b
+                                .getUserFirst()
+                                .getUserName())
+                        .get();
+                var userSecond = userRepository
+                        .findById(b
+                                .getUserSecond()
+                                .getUserName())
+                        .get();
+                String text = "В процессе нападения ";
+                if (b.getTime() > Battle.END) {
+                    text = "Конец победил ";
+                    var i = random.nextInt();
+                    boolean win = i % 2 == 0;
+                    if (win) {
+                        text += userFirst.getUserName();
+                        userFirst.setWins(userFirst.getWins() + 1);
+                        userFirst.setCash(userFirst.getCash() + 100);
+                        userSecond.setCash(userSecond.getCash() - 100);
+                        userSecond.setLoose(userSecond.getLoose() + 1);
+                    } else {
+                        text += userSecond.getUserName();
+                        userSecond.setWins(userSecond.getWins() + 1);
+                        userSecond.setCash(userSecond.getCash() + 100);
+                        userFirst.setCash(userFirst.getCash() - 100);
+                        userFirst.setLoose(userFirst.getLoose() + 1);
+                    }
+                    userRepository.save(userSecond);
+                    userRepository.save(userFirst);
+                    text += userFirst;
+                    text += userSecond;
+                    remove.add(b);
                 }
-                userRepository.save(userSecond);
-                userRepository.save(userFirst);
-                remove.add(b);
-            }
-            SendMessage message =
-                    new SendMessage (userFirst.getChatId(),
-                            text + userFirst);
-            try {
-                writeReadBot
-                        .execute(message);
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
-            message =
-                    new SendMessage (userSecond.getChatId(),
-                            text + userFirst);
-            try {
-                writeReadBot
-                        .execute(message);
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
-        });
-        remove.forEach(x ->
-                battleRepository.delete(x));
+                SendMessage message =
+                        new SendMessage(userFirst.getChatId(),
+                                text);
+                try {
+                    writeReadBot
+                            .execute(message);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+                message =
+                        new SendMessage(userSecond.getChatId(),
+                                text);
+                try {
+                    writeReadBot
+                            .execute(message);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            });
+            remove.forEach(x ->
+                    battleRepository.delete(x));
+        }
     }
 }
